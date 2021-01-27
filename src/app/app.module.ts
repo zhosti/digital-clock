c27820f42621bbf555feafdf366dd9b6de950dc0
izhostov <ivan.zhostov@techture.io>
2021-01-27 22:54:26 +0200

    commit uncommited changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DigitalClockComponent } from './digital-clock/digital-clock.component';
+import { TimeFormatPipe } from './pipes/time-format.pipe';
+import { FormsModule } from '@angular/forms';
+import { DigitBoxComponent } from './digital-clock/digit-box/digit-box.component';
+import { TimeFormatDropdownComponent } from './digital-clock/time-format-dropdown/time-format-dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DigitalClockComponent,
+    TimeFormatPipe,
+    DigitBoxComponent,
+    TimeFormatDropdownComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [TimeFormatPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
